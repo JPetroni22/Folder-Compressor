0b799c83fddc421c0ff0f9e6b8500a3940824cbd
JPetroni22 <135925940+JPetroni22@users.noreply.github.com>
2026-06-26 13:05:38 -0400

    Update compressor.bat

--- compressor.bat
+++ compressor.bat
@@ -1,28 +1,28 @@
 @echo off
 
 echo ====================================================================
-echo        COMPACTADOR DE PASTAS
+echo        FOLDER COMPRESSOR
 echo ====================================================================
 echo.
-echo Este script ira compactar todas as pastas dentro deste diretorio.
-echo Cada pasta nesse diretorio vira 1.zip diferente
+echo This script will compress all folders in this directory.
+echo Each folder will become a separate .zip file
 echo.
-echo INSTRUCOES:
-echo - Coloque este arquivo dentro da pasta
-echo - Execute com duplo clique
-echo - Aguarde a finalizacao
+echo INSTRUCTIONS:
+echo - Place this file inside the folder
+echo - Double-click to run
+echo - Wait for completion
 echo.
 echo ====================================================================
 echo.
 
 pause
 
-REM ----- EXECUCAO -----
+REM ----- EXECUTION -----
 
 for /d %%X in (*) do (
     "C:\Program Files\7-Zip\7z.exe" a "%%X.zip" "%%X\"
 )
 
 echo.
-echo Compactacao concluida!
+echo Compression completed!
 pause
